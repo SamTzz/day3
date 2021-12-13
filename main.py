613b24e8c50d2b106db1ce36ef1cd142499229c6
SamTzz <94377627+SamTzz@users.noreply.github.com>
2021-12-13 17:22:17 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    f = open("input.txt","r")
+    data = f.read().split("\n")
+
+    gamma = []
+    epsilon = []
+
+
+    for nstring in data:
+        print(list(nstring))
+        ListSt = []
+        ListSt.append(list(nstring))
+
+    for n in ListSt:
+        
+        print(n)
